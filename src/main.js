a2c2f9e15a57456a19f714b59c85ba2b6f6bd2e0
naia <x1039334067@163.com>
2022-01-15 15:41:06 +0800

    引入了Vant组件库

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
 // 加载全局样式
 import './styles/index.less'
+
+// 注册使用Vant组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
